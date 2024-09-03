77b11864e0e134c9af7d8ec6942cdf5ee19f1f65
khakhiD <kakkiid@gmail.com>
2024-09-03 23:03:27 +0900

    feat: IdeaSection 구현 완료

--- src/components/sections/IdeaSection.vue
+++ src/components/sections/IdeaSection.vue
@@ -3,20 +3,38 @@
     id="idea-section"
     class="relative w-svw h-svh bg-background transition-all">
     <div
-      class="w-full h-full flex items-center px-4 md:px-20"
+      class="w-full h-full flex items-center overflow-hidden px-4 md:px-20"
       :class="CSS_ANIMATION">
       <div>
         <h1
-          class="font-Poppins font-semibold mb-7 text-[32px] whitespace-pre-line md:text-[80px]">
+          class="font-Poppins font-semibold mb-7 md:mb-0 text-[32px] whitespace-pre-line md:text-[80px]">
           {{ CONSTANTS_IDEA.TITLE }}
         </h1>
         <p
-          class="font-Poppins font-normal text-base whitespace-pre-line ease md:text-[20px] md:leading-tight">
+          class="font-Poppins font-normal text-base whitespace-pre-line ease md:text-[20px] md:leading-normal">
           {{ CONSTANTS_IDEA.CONTENT }}
         </p>
-        <div class="block md:hidden">📼 logo video (mobile)</div>
+        <div class="block md:hidden w-full z-10 pt-[60px]">
+          <video
+            autoplay
+            muted
+            loop
+            playinline
+            class="w-full scale-[2] object-contain">
+            <source :src="CONSTANTS_IDEA.VIDEO_URL" type="video/webm" />
+          </video>
+        </div>
       </div>
-      <div class="hidden md:block">📼 logo video (pc)</div>
+      <div class="hidden md:block">
+        <video
+          autoplay
+          muted
+          loop
+          playsinline
+          class="w-full max-w-7xl z-10 md:right-0 md:absolute md:top-1/2 md:left-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2 pt-32 md:pt-0">
+          <source :src="CONSTANTS_IDEA.VIDEO_URL" type="video/webm" />
+        </video>
+      </div>
     </div>
   </section>
 </template>
@@ -32,7 +50,7 @@ const CONSTANTS_IDEA = {
     enjoy what they can. we can
     enjoy what we can do well.
   `,
-  VIDEO_URL: `https://amuz.co.kr/themes/web/official/videos/amuz_L.webm`,
+  VIDEO_URL: `/amuz/amuz_L.webm`,
 };
 
 const CSS_ANIMATION = ref('transition-all ease-in-out duration-500');
