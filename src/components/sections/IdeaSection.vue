f59ba3aebfafa5e74370dccb813f54f6513e64c1
Dongho Shin <kakkiid@gmail.com>
2024-09-03 16:56:45 +0900

    feat: IdeaSection 구조 구현

--- src/components/sections/IdeaSection.vue
+++ src/components/sections/IdeaSection.vue
@@ -1,7 +1,28 @@
 <template>
-  <div></div>
+  <section id="idea-section" class="relative">
+    <div class="">
+      <div>
+        <h1>{{ CONSTANTS_IDEA.TITLE }}</h1>
+        <p>{{ CONSTANTS_IDEA.CONTENT }}</p>
+        <div class="">logo video mobile</div>
+      </div>
+      <div id="amuz-logo">logo video pc</div>
+    </div>
+  </section>
 </template>
 
-<script setup></script>
+<script setup>
+const CONSTANTS_IDEA = {
+  TITLE: `
+    Idea can change
+    everything
+  `,
+  CONTENT: `
+    start-up companies can only
+    enjoy what they can. we can
+    enjoy what we can do well.
+  `,
+};
+</script>
 
 <style lang="scss" scoped></style>
