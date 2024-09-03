a25aa82b1ed2518f706a910d0c4e14ca91f1b904
khakhiD <kakkiid@gmail.com>
2024-09-03 22:21:58 +0900

    chore: 테스트용 코드 주석처리

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@ import LandingPage from './views/LandingPage.vue';
 </script>
 
 <template>
-  <small class="text-[36px] text-background font-extrabold">App.vue</small>
+  <!-- TODO: 테스트용 코드 제거하기 -->
+  <!-- <small class="text-[36px] text-background font-extrabold">App.vue</small> -->
   <LandingPage />
 </template>
 
