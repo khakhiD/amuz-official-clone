658a8a57f68e6fe0180fd393abe85c701e24e02d
khakhiD <kakkiid@gmail.com>
2024-09-03 22:14:27 +0900

    style: Header 스타일링

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,11 @@
 <template>
   <header
     id="Header"
-    class="sticky top-0 w-full px-[80px] z-50 backdrop-blur-[30px]">
+    class="sticky top-0 w-full px-4 md:px-20 py-6 z-50 h-20 backdrop-blur-[30px]">
     <div class="flex justify-between items-center">
-      <a href="">amuz</a>
+      <a href="/">amuz</a>
       <nav>
-        <button>menu</button>
+        <button class="w-10 h-10 relative foucs:outline-none">menu</button>
       </nav>
     </div>
   </header>
