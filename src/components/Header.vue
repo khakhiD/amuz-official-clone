dab32b9232fc4366353d981294c6ea4a8589c5ac
Dongho Shin <kakkiid@gmail.com>
2024-09-03 16:30:51 +0900

    feat: Header 구조 구현

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,14 @@
 <template>
-  <div></div>
+  <header
+    id="Header"
+    class="sticky top-0 w-full px-[80px] z-50 backdrop-blur-[30px]">
+    <div class="flex justify-between items-center">
+      <a href="">amuz</a>
+      <nav>
+        <button>menu</button>
+      </nav>
+    </div>
+  </header>
 </template>
 
 <script setup></script>
-
-<style lang="scss" scoped></style>
