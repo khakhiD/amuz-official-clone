a9f019315cb9563deade0618bc2e8268490e1ce7
Dongho Shin <kakkiid@gmail.com>
2024-09-03 12:28:29 +0900

    init: vite + vue + ts + tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/components/**/*.{html,js,ts,vue}'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extends: {},
+  },
+  plugins: [],
+};
