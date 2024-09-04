913e408d7ca989ce989a5800ea23157ac3d67196
Dongho Shin <kakkiid@gmail.com>
2024-09-04 15:21:46 +0900

    feat: PointSection 구조 구현

--- src/components/sections/PointSection.vue
+++ src/components/sections/PointSection.vue
@@ -1,7 +1,34 @@
 <template>
-  <div></div>
+  <secion
+    class="h-lvh flex items-center justify-center text-white py-6 px-4 md:max-w-7xl md:py-0 md:px-20 md:mx-auto">
+    <div class="">
+      <!-- for large screen -->
+      <h1 class="text-[26px] md:text-[53px] font-bold">
+        <span>
+          {{ PRAISE_LINES.FIRST }}
+          <img :src="IMAGES_URL.YELLOW" class="h-10 md:h-20 inline" />
+          {{ PRAISE_LINES.SECOND }}
+          <img :src="IMAGES_URL.BLUE" class="h-10 md:h-20 inline" />
+          {{ PRAISE_LINES.THIRD }}
+        </span>
+      </h1>
+      <!-- common component: button -->
+      <button>About us</button>
+    </div>
+  </secion>
 </template>
 
-<script setup></script>
+<script setup>
+const PRAISE_LINES = {
+  FIRST: '시작하는 기업은 잘할 수 있는',
+  SECOND: '일을 즐겁게 하도록, 우리들은 우리가',
+  THIRD: '잘할 수 있는 일을 즐겁게 하도록. 이것이 아뮤즈의 핵심가치입니다.',
+};
+
+const IMAGES_URL = {
+  YELLOW: '/amuz/amuz02.png',
+  BLUE: '/amuz/amuz03.png',
+};
+</script>
 
 <style lang="scss" scoped></style>
