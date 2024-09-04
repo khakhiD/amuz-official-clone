ee7caa07a017b943287ac9a00920a00d2095a7d5
Dongho Shin <kakkiid@gmail.com>
2024-09-04 15:42:20 +0900

    feat: WorkSection 구조 구현

--- src/components/sections/WorkSection.vue
+++ src/components/sections/WorkSection.vue
@@ -1,5 +1,44 @@
 <template>
-  <div></div>
+  <!-- NOTE: Work Archive Section -->
+  <section>
+    <div>
+      <h1>WORK ARCHIVE</h1>
+      <div id="works-array">
+        <!-- items -->
+        <!-- NOTE: a 태그의 background-image로 아이템 이미지 넣기 -->
+        <a href="">
+          <div class="item-info">
+            <small>app</small>
+            <p>MY CUP</p>
+          </div>
+        </a>
+        <a href="">
+          <div class="item-info">
+            <small>app</small>
+            <p>LAUNDRY CREW</p>
+          </div>
+        </a>
+        <a href="">
+          <div class="item-info">
+            <small>Front-end</small>
+            <p>LG ThinQ</p>
+          </div>
+        </a>
+        <a href="">
+          <div class="item-info">
+            <small>app</small>
+            <p>ECO CLEAN</p>
+          </div>
+        </a>
+        <a href="">
+          <div class="item-info">
+            <small>web</small>
+            <p>GRAB THE OCEAN</p>
+          </div>
+        </a>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup></script>
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,12 +1,14 @@
 <template>
   <DefaultLayout>
     <IdeaSection />
+    <PointSection />
   </DefaultLayout>
 </template>
 
 <script setup>
 import DefaultLayout from '../layouts/DefaultLayout.vue';
 import IdeaSection from '../components/sections/IdeaSection.vue';
+import PointSection from '../components/sections/PointSection.vue';
 </script>
 
 <style lang="scss" scoped></style>
