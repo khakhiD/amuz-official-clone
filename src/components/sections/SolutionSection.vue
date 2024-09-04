a49699a4346e8ed669510346ed57fb1d21c91b55
Dongho Shin <kakkiid@gmail.com>
2024-09-04 15:44:08 +0900

    feat: SolutionSection 구조 구현
    Service -> Solution으로 Rename

---
+++ src/components/sections/SolutionSection.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <!-- wrapper -->
+    <div>
+      <!-- scroll animation main -->
+      <div>
+        <!-- Box || Card Components -->
+        <div>SENDGO</div>
+        <div>CMS2.5</div>
+        <div>STOREUP</div>
+        <div>Rent-ve</div>
+      </div>
+      <!-- background title & button -->
+      <div>
+        <h1 class="whitespace-pre-line">{{ TITLE_CONTENT }}</h1>
+        <button>View More</button>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+const TITLE_CONTENT = `
+  INNOVATIVE
+  SERVICES
+`;
+</script>
+
+<style lang="scss" scoped></style>
