26a7ee8e866a8a5db97dfd7a57d620f5e1b8f167
Dongho Shin <kakkiid@gmail.com>
2024-09-05 20:11:35 +0900

    feat: WorkCard 마우스 호버링 시 tooltip 표시 기능 추가

--- src/components/WorkCard.vue
+++ src/components/WorkCard.vue
@@ -2,7 +2,9 @@
 <template>
   <a
     :href
-    class="flex relative items-end bg-repeat bg-center bg-cover w-full h-[380px] p-6 md:p-[3vw] md:w-[56vw] md:h-[31vw] rounded-[5vw]"
+    @mousemove="onMouseMove"
+    @mouseleave="onMouseLeave"
+    class="flex relative items-end bg-repeat bg-center bg-cover cursor-pointer w-full h-[380px] p-6 md:p-[3vw] md:w-[56vw] md:h-[31vw] rounded-[5vw]"
     :style="{ backgroundImage: `url(${imageSrc})` }">
     <div aria-label="info">
       <p class="text-sm">{{ category }}</p>
@@ -10,11 +12,22 @@
         {{ title }}
       </p>
       <p class="md:hidden">{{ description }}</p>
+      <div
+        id="work-tooltip"
+        v-if="isHovering"
+        :style="{ top: `${y}px`, left: `${x}px` }"
+        class="hidden md:block py-4 px-[30px] fixed max-w-fit bg-background rounded-[28px] bg-opacity-50 z-20 pointer-events-none transition duration-100 ease-out">
+        <p class="font-medium whitespace-nowrap overflow-hidden">
+          {{ description }}
+        </p>
+      </div>
     </div>
   </a>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
 interface Props {
   category: string;
   title: string;
@@ -25,4 +38,18 @@ interface Props {
 
 const props = defineProps<Props>();
 const { category, title, imageSrc, description, href } = props;
+
+const x = ref(0);
+const y = ref(0);
+const isHovering = ref(false);
+
+const onMouseMove = (e: MouseEvent) => {
+  x.value = e.clientX;
+  y.value = e.clientY;
+  isHovering.value = true;
+};
+
+const onMouseLeave = () => {
+  isHovering.value = false;
+};
 </script>
