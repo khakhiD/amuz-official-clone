2c60f89c0c0cf7c99dc1e522ec4babc122eb8da3
Dongho Shin <kakkiid@gmail.com>
2024-09-05 17:11:50 +0900

    feat: LandingPage에 각 섹션 컴포넌트 렌더링

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -2,6 +2,9 @@
   <DefaultLayout>
     <IdeaSection />
     <PointSection />
+    <WorkSection />
+    <SolutionSection />
+    <!-- <ContactSection /> -->
   </DefaultLayout>
 </template>
 
@@ -9,6 +12,9 @@
 import DefaultLayout from '../layouts/DefaultLayout.vue';
 import IdeaSection from '../components/sections/IdeaSection.vue';
 import PointSection from '../components/sections/PointSection.vue';
+import WorkSection from '../components/sections/WorkSection.vue';
+import SolutionSection from '../components/sections/SolutionSection.vue';
+import ContactSection from '../components/sections/ContactSection.vue';
 </script>
 
 <style lang="scss" scoped></style>
