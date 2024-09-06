763ed2b8f5ba0e43a5414ddc34d154e2d39b8792
Dongho Shin <kakkiid@gmail.com>
2024-09-06 15:11:11 +0900

    feat: ContactSection 구조 및 스타일링 구현

--- src/components/sections/ContactSection.vue
+++ src/components/sections/ContactSection.vue
@@ -1,16 +1,17 @@
 <template>
-  <section>
-    <!-- wrapper -->
+  <section
+    class="flex h-screen md:h-screen md:w-screen items-center justify-center md:justify-center">
     <div>
-      <!-- foreground title & button -->
-      <div>
-        <h1>{{ TITLE_CONTENT }}</h1>
-        <button>Contact us</button>
+      <!-- TEXT -->
+      <div class="px-4 md:px-0 z-10 text-center">
+        <h1 class="mb-5 md:mb-[70px] font-semibold text-xl md:text-[56px]">
+          <span>{{ TITLE_CONTENT }}</span>
+        </h1>
+        <Button v-bind="buttonProps" />
       </div>
-      <!-- scroll animation main -->
       <div>
-        <!-- 8 icons-->
-        <!-- NOTE: 정중앙 기준 -> 스크롤할 수록 확대 + 투명도 조절 -->
+        <!-- NOTE: 8개의 요소 애니메이션
+        정중앙 기준 -> 스크롤할 수록 확대 + 투명도 조절 -->
         <div v-for="n in 8" :key="n" class="TEMP_ICONS_BASECLASS"></div>
       </div>
     </div>
@@ -18,8 +19,15 @@
 </template>
 
 <script setup>
+import Button from '../Button.vue';
+
+const buttonProps = {
+  content: 'Contact us',
+  href: null,
+};
+
 const TITLE_CONTENT = '즐거운 상상은 현실이됩니다.';
-const TEMP_ICONS_BASECLASS = 'w-5 h-5 bg-red';
+const TEMP_ICONS_BASECLASS = '';
 </script>
 
 <style lang="scss" scoped></style>
