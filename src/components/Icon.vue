afc991655067fceb89dd264dd58fda04a860376e
Dongho Shin <kakkiid@gmail.com>
2024-09-12 00:12:04 +0900

    feat: Icon 공통 컴포넌트 구현

---
+++ src/components/Icon.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="box"
+    :style="{ background: `url(${imageSrc}) center center / cover no-repeat` }"
+  </div>
+</template>
+<script setup lang="ts">
+export interface IconProps {
+  imageSrc: string;
+}
+
+const props = defineProps<IconProps>();
+const { imageSrc } = props;
+
+</script>
