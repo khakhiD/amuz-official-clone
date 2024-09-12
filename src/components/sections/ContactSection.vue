dd614d6020557c1f8b438dd702f7f20a2c0bc06d
Dongho Shin <kakkiid@gmail.com>
2024-09-12 15:59:20 +0900

    style: overflowing되지 않게 아이콘 사이즈 수정

--- src/components/sections/ContactSection.vue
+++ src/components/sections/ContactSection.vue
@@ -1,7 +1,6 @@
 <template>
   <section
     class="relative flex h-screen md:h-screen md:w-screen items-center justify-center md:justify-center">
-    <!-- NOTE: 텍스트 들어갈 곳 -->
     <div
       class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 text-center w-full">
       <h1 class="mb-5 md:mb-[70px] font-semibold text-xl md:text-[56px]">
@@ -9,13 +8,12 @@
       </h1>
       <Button v-bind="buttonProps" />
     </div>
-    <!-- NOTE: 8개의 요소 애니메이션 들어갈 곳 -->
-    <div class="absolute w-screen h-60vw md:h-screen">
+    <div class="absolute w-screen h-[60vw] md:h-screen">
       <div
-        v-for="(item, index) in ICON_IMAGES"
+        v-for="(icon, index) in ICONS"
         :key="index"
-        :class="`icon-box icon-box-${index + 1}`">
-        <Icon :imageSrc="item.imageSrc" :class="`icon icon-${index + 1}`" />
+        :class="`absolute ${icon.baseClass}`">
+        <Icon :imageSrc="icon.imageSrc" :class="`scale-150`" />
       </div>
     </div>
   </section>
@@ -32,16 +30,25 @@ const buttonProps = {
   href: null,
 };
 
-const ICON_IMAGES = [
-  { imageSrc: '/amuz/amuz13.png' },
-  { imageSrc: '/amuz/amuz14.png' },
-  { imageSrc: '/amuz/amuz15.png' },
-  { imageSrc: '/amuz/amuz16.png' },
-  { imageSrc: '/amuz/amuz17.png' },
-  { imageSrc: '/amuz/amuz18.png' },
-  { imageSrc: '/amuz/amuz19.png' },
-  { imageSrc: '/amuz/amuz20.png' },
-  { imageSrc: '/amuz/amuz21.png' },
+const ICONS = [
+  { imageSrc: '/amuz/amuz13.png', baseClass: 'top-0 left-[35%]' },
+  { imageSrc: '/amuz/amuz14.png', baseClass: 'top-[5%] left-0 scale-150' },
+  { imageSrc: '/amuz/amuz15.png', baseClass: 'top-[30%] left-[10%]' },
+  { imageSrc: '/amuz/amuz16.png', baseClass: 'bottom-[20%] left-0 scale-150' },
+  { imageSrc: '/amuz/amuz17.png', baseClass: 'top-[15%] right-[35%] scale-50' },
+  { imageSrc: '/amuz/amuz18.png', baseClass: 'top-0 right-[10%] scale-[2.0]' },
+  {
+    imageSrc: '/amuz/amuz19.png',
+    baseClass: 'right-[10%] top-[20%] scale-[0.8]',
+  },
+  {
+    imageSrc: '/amuz/amuz20.png',
+    baseClass: 'right-[10%] bottom-[20%] scale-[0.5]',
+  },
+  {
+    imageSrc: '/amuz/amuz21.png',
+    baseClass: 'right-[30%] bottom-[5%] scale-150',
+  },
 ];
 
 const TITLE_CONTENT = '즐거운 상상은 현실이됩니다.';
@@ -54,66 +61,3 @@ onMounted(() => {
   }
 });
 </script>
-
-<style lang="css" scoped>
-.icon-box {
-  position: absolute;
-  filter: brightness(100%);
-}
-.icon {
-  opacity: 1;
-  transform: scale(1.5, 1.5);
-}
-
-.icon-box-1 {
-  top: 0%;
-  left: 35%;
-}
-
-.icon-box-2 {
-  top: 5%;
-  left: 0%;
-  transform: scale(1.5, 1.5);
-}
-
-.icon-box-3 {
-  top: 30%;
-  left: 10%;
-}
-
-.icon-box-4 {
-  bottom: 20%;
-  left: 0%;
-  transform: scale(1.5, 1.5);
-}
-
-.icon-box-5 {
-  transform: scale(0.5, 0.5);
-  top: 15%;
-  right: 35%;
-}
-
-.icon-box-6 {
-  right: 0;
-  top: 0;
-  transform: scale(2, 2);
-}
-
-.icon-box-7 {
-  right: 10%;
-  top: 20%;
-  transform: scale(0.8, 0.8);
-}
-
-.icon-box-8 {
-  right: 10%;
-  bottom: 5%;
-  transform: scale(1.5, 1.5);
-}
-
-.icon-box-9 {
-  bottom: 0;
-  right: 30%;
-  transform: scale(0.8, 0.8);
-}
-</style>
