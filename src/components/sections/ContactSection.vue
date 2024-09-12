8cfe2189fa688ac81a0f0b6cf66b42aadb252387
Dongho Shin <kakkiid@gmail.com>
2024-09-12 16:31:57 +0900

    feat: ContactSection expand 애니메이션 적용

--- src/components/sections/ContactSection.vue
+++ src/components/sections/ContactSection.vue
@@ -1,6 +1,7 @@
 <template>
   <section
-    class="relative flex h-screen md:h-screen md:w-screen items-center justify-center md:justify-center">
+    class="relative flex h-screen md:h-screen md:w-screen items-center justify-center md:justify-center"
+    ref="container">
     <div
       class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 text-center w-full">
       <h1 class="mb-5 md:mb-[70px] font-semibold text-xl md:text-[56px]">
@@ -8,7 +9,7 @@
       </h1>
       <Button v-bind="buttonProps" />
     </div>
-    <div class="absolute w-screen h-[60vw] md:h-screen">
+    <div class="absolute w-screen h-[60vw] md:h-screen" ref="iconContainer">
       <div
         v-for="(icon, index) in ICONS"
         :key="index"
@@ -23,7 +24,7 @@
 import { onMounted, ref } from 'vue';
 import Button from '../Button.vue';
 import Icon from '../Icon.vue';
-import { textFillAndSpacing } from '../../utils/animations';
+import { expandIcons, textFillAndSpacing } from '../../utils/animations';
 
 const buttonProps = {
   content: 'Contact us',
@@ -54,10 +55,15 @@ const ICONS = [
 const TITLE_CONTENT = '즐거운 상상은 현실이됩니다.';
 
 const titleRef = ref<HTMLElement | null>(null);
+const iconContainer = ref<HTMLElement | null>(null);
+const container = ref<HTMLElement | null>(null);
 
 onMounted(() => {
   if (titleRef.value) {
     textFillAndSpacing(titleRef.value);
+  }
+  if (iconContainer.value) {
+    expandIcons(container.value, iconContainer.value);
   }
 });
 </script>
